24a4a85915c3957265a781d6fe459df181420870
Zero-Set <stonickers@gmail.com>
2026-04-26 19:44:53 +0900

    resetAllHistoryの移動

--- assets/script.js
+++ assets/script.js
@@ -160,11 +160,3 @@ function showResult() {
     retryBtn.classList.add("hidden");
   }
 }
-
-function resetAllHistory() {
-  if (confirm("記録をすべて削除しますか？")) {
-    localStorage.removeItem(STORAGE_KEY);
-    alert("記録を削除しました。");
-    location.reload();
-  }
-}
